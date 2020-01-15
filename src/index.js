9f946b267e7b1e9e8fc5318e09fa7677d36195c3
zhifez <zhifez.lee@gmail.com>
2020-01-15 17:42:02 +0800

    Added HashRouter to indexjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -11,7 +12,9 @@ const store = storeConfig ();
 
 ReactDOM.render (
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>, 
   document.getElementById ( 'root' ) 
 );

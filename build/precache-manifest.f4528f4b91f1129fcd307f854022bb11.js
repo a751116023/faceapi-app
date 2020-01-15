63d0dfad54540481d671dd4c95228302ef9cc53e
zhifez <zhifez.lee@gmail.com>
2020-01-15 17:22:05 +0800

    New build

---
+++ build/precache-manifest.f4528f4b91f1129fcd307f854022bb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0d525ba803d9bdb9700ea36b503b13",
+    "url": "/faceapi-app/index.html"
+  },
+  {
+    "revision": "94154d7ebd7da614935a",
+    "url": "/faceapi-app/static/css/main.af1bc6e4.chunk.css"
+  },
+  {
+    "revision": "eec17a4de3188afd45cc",
+    "url": "/faceapi-app/static/js/2.acac4895.chunk.js"
+  },
+  {
+    "revision": "3408a2a77c882691491f0c2246b0afbc",
+    "url": "/faceapi-app/static/js/2.acac4895.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94154d7ebd7da614935a",
+    "url": "/faceapi-app/static/js/main.b9bdcef6.chunk.js"
+  },
+  {
+    "revision": "c0717f3190bc869555bb",
+    "url": "/faceapi-app/static/js/runtime-main.78dcd928.js"
+  }
+]);

eec8b7aa4b7dde36cebe35c65748672db9bcb497
zhifez <zhifez.lee@gmail.com>
2020-01-15 17:09:34 +0800

    - Added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import './App.css';
+import createHistory from 'history/createBrowserHistory';
 
+import './App.css';
 import Home from './pages/home/home';
 import SetupFaceId from './pages/setupFaceId/setupFaceId';
 import Error from './components/error/error';
@@ -9,7 +10,7 @@ import Error from './components/error/error';
 const App = () => {
   return (
     <div className="App">
-      <Router>
+      <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
         <Home path="/home" />
         <SetupFaceId path="/" />
         <Error default />
